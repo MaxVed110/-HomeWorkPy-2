99245f08e035d919693a6f01313dc8e2f78cdde0
Maxim <vedernikovmaxim@rambler.ru>
2022-05-28 16:01:30 +0300

    Решена первая задача

---
+++ Задача_1_сумма_нечётных_номеров/program.py
@@ -0,0 +1,14 @@
+#Найти сумму чисел списка стоящих на нечетной позиции
+#Пример:[1,2,3,4] -> 4
+
+
+def SumOfOddPositions(list):
+    sum = 0
+    for i in range(0, list.__len__(), 2):
+        sum += list[i]
+    return sum
+
+
+listUser = [1,2,3,4, 5]
+sumUser = SumOfOddPositions(listUser)
+print(f"Сумма чисел списка, стоящих на нечетной позиции, равна: {sumUser}")
