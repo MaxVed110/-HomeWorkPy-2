b3b3bad433ba9217304790339fb680e3a56ed9f4
Maxim <vedernikovmaxim@rambler.ru>
2022-05-28 16:20:12 +0300

    Решена вторая задача

---
+++ Задача_2_произведение_пар_чисел_в_списке/program.py
@@ -0,0 +1,18 @@
+#Найти произведение пар чисел в списке. Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+#Пример: [2, 3, 4, 5, 6] => [12, 15, 16]; [2, 3, 5, 6] => [12, 15] 
+
+def  ProductOfPairsOfNumbersInTheList(list):
+    productNumbersList = []
+    if(list.__len__()%2==0):
+        for i in range(int(list.__len__()/2)):
+            productNumbersList.append(list[i]*list[list.__len__()-1-i]) 
+    else:
+        for i in range(int(list.__len__()/2)):
+            productNumbersList.append(list[i]*list[list.__len__()-1-i]) 
+        productNumbersList.append(list[int(list.__len__()/2)+1]**2)
+    return productNumbersList
+
+
+listUser = [2, 3, 4, 5, 6]
+productUserList = ProductOfPairsOfNumbersInTheList(listUser)
+print(f"Произведение пар чисел в списке равно: {productUserList}")
